03c07b937bc902828239e79a6abe26f1eb6d982a
Liang Chun <liangchun@TypeError-MBP.local>
2017-04-02 12:08:30 +0200

    Initial Commit

---
+++ nightwatch.conf.js
@@ -0,0 +1,77 @@
+const PKG = require('./package.json'); // so we can get the version of the project
+const BINPATH = './node_modules/nightwatch/bin/'; // change if required.
+const SCREENSHOT_PATH = "./screenshots/" + PKG.version + "/";
+
+const config = { 
+    "src_folders": [
+        "tests"
+    ],
+    "output_folder": "./reports",
+    "selenium": {
+        "start_process": true,
+        "server_path": BINPATH + "selenium.jar",
+        "log_path": "",
+        "host": "127.0.0.1",
+        "port": 4444,
+        "cli_args": {
+            "webdriver.chrome.driver": BINPATH + "chromedriver" 
+        }
+    },
+    "test_workers": { "enabled": true, "workers": "auto" },
+    "test_settings": {
+        "default": {
+            "launch_url": "http://localhost",
+            "selenium_port": 4444,
+            "selenium_host": "127.0.0.1",
+            "silent": true,
+            "screenshots": {
+                "enabled": true,
+                "path": SCREENSHOT_PATH
+            },
+            "globals": {
+                "waitForConditionTimeout": 15000
+            },
+            "desiredCapabilities": {
+                "browserName": "chrome",
+                "javascriptEnabled": true,
+                "acceptSslCerts": true
+            }
+        }
+    }
+}
+module.exports = config;
+
+
+require('fs').stat(BINPATH + 'selenium.jar', function(err, stat) { // got it?
+    if (err || !stat || stat.size < 1) {
+        require('selenium-download').ensure(BINPATH, function(error) {
+            if (error) throw new Error(error); // no point continuing so exit!
+            console.log('✔ Selenium & Chromedriver downloaded to:', BINPATH);
+        });
+    }
+});
+
+function padLeft(count) { // theregister.co.uk/2016/03/23/npm_left_pad_chaos/
+    return count < 10 ? '0' + count : count.toString();
+}
+
+var FILECOUNT = 0; // "global" screenshot file count
+/**
+ * The default is to save screenshots to the root of your project even though
+ * there is a screenshots path in the config object above! ... so we need a
+ * function that returns the correct path for storing our screenshots.
+ * While we're at it, we are adding some meta-data to the filename, specifically
+ * the Platform/Browser where the test was run and the test (file) name.
+ */
+function imgpath(browser) {
+    var a = browser.options.desiredCapabilities;
+    var meta = [a.platform];
+    meta.push(a.browserName ? a.browserName : 'any');
+    meta.push(a.version ? a.version : 'any');
+    meta.push(a.name); // this is the test filename so always exists.
+    var metadata = meta.join('~').toLowerCase().replace(/ /g, '');
+    return SCREENSHOT_PATH + metadata + '_' + padLeft(FILECOUNT++) + '_';
+}
+
+module.exports.imgpath = imgpath;
+module.exports.SCREENSHOT_PATH = SCREENSHOT_PATH;
---
+++ tests/google.js
@@ -0,0 +1,17 @@
+var config = require('../nightwatch.conf.js');
+
+module.exports = {
+  'Demo test Google' : function (browser) {
+    browser
+      .url('http://www.google.com')
+      .waitForElementVisible('body', 1000)
+      .assert.title('Google')
+      .assert.visible('input[type=text]')
+      .setValue('input[type=text]', 'rembrandt van rijn')
+      .waitForElementVisible('button[name=btnG]', 1000)
+      .click('button[name=btnG]')
+      .pause(1000)
+      .saveScreenshot(config.imgpath(browser) + 'google.png')
+      .end();
+  }
+};
